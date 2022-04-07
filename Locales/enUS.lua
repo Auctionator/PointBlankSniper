e0050539763a3594253f1cfbe368e6998e1c1bb1
plusmouse <plusmouse@protonmail.com>
2022-04-07 16:59:39 +0100

    tweak name in locales

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -2,7 +2,7 @@ POINT_BLANK_SNIPER_LOCALES.enUS = function()
   local L = {}
 
   L["TAB"] = "Snipe"
-  L["TAB_HEADER"] = "Point-Blank Sniper"
+  L["TAB_HEADER"] = "Point Blank Sniper"
   L["START"] = "Start"
   L["STOP"] = "Stop"
   L["SHOPPING_LIST_NAME_COLON"] = "Shopping List Name:"
