f4d841a2b25033c13723233cdc1196c2cbda4e33
plusmouse <plusmouse@protonmail.com>
2022-04-07 06:27:45 +0100

    Fix configs resetting on load

--- Source/Tab/Main.lua
+++ Source/Tab/Main.lua
@@ -14,10 +14,11 @@ function PointBlankSniperTabFrameMixin:OnLoad()
 
   self.ResultsListing:Init(self.DataProvider)
   self.ListName:SetText(POINT_BLANK_SNIPER_CURRENT_LIST)
-  self:UpdateStartButton()
-  self:UpdateConfigs()
   self.UseBleep:SetChecked(not POINT_BLANK_SNIPER_DISABLE_BLEEP)
   self.UseFlash:SetChecked(not POINT_BLANK_SNIPER_DISABLE_FLASH)
+
+  self:UpdateStartButton()
+  self:UpdateConfigs()
 end
 
 function PointBlankSniperTabFrameMixin:UpdateStartButton()
